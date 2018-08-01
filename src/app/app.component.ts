8ac3614049b1e887bc1dc247265bb63a1ef39c8e
Bendixsen, Jeff <jbendixsen@tmwsystems.com>
2018-08-01 15:36:17 -0700

    Core functionality working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Native
 })
 export class AppComponent {
-  title = 'tm-search-element';
+  @Input() name = 'friend';
+  focus: string;
+  focusSet = false;
+  setFocus(value) {
+    this.focus = value;
+    this.focusSet = true;
+  }
 }
